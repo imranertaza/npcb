ee7f35ac1f0676c57ee0595f7ab49b65d75eea07
dnationsoftdm6 <dnationsoftdm6@gmail.com>
2026-01-04 20:53:44 +0600

    added optional og title

--- resources/views/news/news-and-updates-details.blade.php
+++ resources/views/news/news-and-updates-details.blade.php
@@ -6,7 +6,7 @@
 @section('meta_keywords', $news->meta_keywords)
 @section('meta_title', $news->meta_title)
 @section('og_image', getImageUrl($news->image ?? $news->f_image))
-@section('og_title', $news->meta_title)
+@section('og_title', $news->meta_title ?? $news->news_title)
 @section('content')
 
     <section class="container py-100">
